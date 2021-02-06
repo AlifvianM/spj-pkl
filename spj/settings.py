c642db9aa53361664904256ec11eab88617a2258
Alifvian Marco <alifvianmarco@Alifvians-MacBook-Air.local>
2021-02-06 08:02:52 +0700

    First again

--- spj/settings.py
+++ spj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3a9&l1hldz-1e^k475^)kp3wr($4-6u0m#@1(r0ff)n7sig#n)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
